fc67e7ded4c0d029a095edb207656c53771053a3
Lucas Zacharias <lucas@zacharias-net.de>
2025-04-29 07:07:55 +0200

    Refactor navigation menu icons for improved consistency and styling

--- resources/views/livewire/user-navigation-menu.blade.php
+++ resources/views/livewire/user-navigation-menu.blade.php
@@ -266,24 +266,18 @@ wire:loading.class="cursor-wait"
                                 {{ __('Home') }}
                             </x-nav-link>
                             <x-nav-link href="/" wire:navigate  :active="request()->is('/')">
-                                    <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                                    </svg>
-
+                                    
+                                    <svg  class="w-4 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
                                 {{ __('Versicherungen') }}
                             </x-nav-link>
                             <x-nav-link href="/" wire:navigate  :active="request()->is('/')">
-                                    <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                                    </svg>
-
+                                    
+                                    <svg class="w-4 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800"  viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                                 {{ __('Bewertungen') }}
                             </x-nav-link>
                             <x-nav-link href="/" wire:navigate  :active="request()->is('/')">
-                                    <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                                    </svg>
-
+                                    
+                                    <svg class="w-4 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800"  viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
                                 {{ __('Ranking ') }}
                             </x-nav-link>
                             
@@ -342,10 +336,7 @@ wire:loading.class="cursor-wait"
                                         </div>
                                 </div>
                                 <x-nav-link href="/" wire:navigate  :active="request()->is('/')">
-                                    <svg class="w-5 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/>
-                                    </svg>
-
+                                    <svg class="w-4 max-md:w-6 aspect-square mr-1 max-md:mr-2 text-gray-800"  viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
                                 {{ __('Premium ') }}
                             </x-nav-link>
                            @auth
