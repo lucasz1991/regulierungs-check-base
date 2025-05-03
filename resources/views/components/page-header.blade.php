6d6c9be4ca9b884a9dd50e2e2596ec89c8ba10a8
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-03 09:40:59 +0200

    Update page header styles for improved layout and background opacity

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,8 +1,8 @@
     @if ($isWebPage && $showHeader)
         <header class="relative bg-cover bg-center min-h-32" 
         style="background-image: url('{{ $header_image ? url('public/' . $header_image) : asset('site-images/background.jpg') }}');">
-        <div class="absolute inset-0 "></div>
-            <div class="relative container mx-auto py-4 text-2xl px-4 sm:px-6 lg:px-8 space-y-12">
+        <div class="absolute inset-0 bg-blue-50 opacity-80"></div>
+            <div class="relative container mx-auto py-12 text-2xl px-4 sm:px-6 lg:px-8 space-x-6 flex justify-start  items-center">
                 <x-back-button />
                 <h1 class="font-semibold text-2xl text-gray-800 leading-tight flex items-center">
                     {{ $title }}
