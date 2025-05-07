9f125745c2cd80d84842100619d774d05d079891
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-07 19:02:18 +0200

    Refactor insurance rating display to use a dedicated component for improved maintainability and visual consistency

--- resources/views/livewire/pages/insurances.blade.php
+++ resources/views/livewire/pages/insurances.blade.php
@@ -30,20 +30,7 @@
                         </div>
                         <div class="flex items-center justify-between">
                             <div>
-                                @php
-                                    $score = round($insurance->claim_ratings_avg_rating_score ?? 0, 1);
-                                    $percentage = ($score / 5) * 100;
-                                @endphp
-
-                                <div class="relative flex items-center">
-                                    <div class="flex w-full text-gray-300">
-                                        @for ($i = 0; $i < 5; $i++)
-                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.957a1 1 0 00.95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 00-.364 1.118l1.286 3.957c.3.921-.755 1.688-1.54 1.118l-3.37-2.448a1 1 0 00-1.176 0l-3.37 2.448c-.784.57-1.838-.197-1.54-1.118l1.286-3.957a1 1 0 00-.364-1.118L2.049 9.384c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 00.95-.69l1.286-3.957z"></path>
-                                            </svg>
-                                        @endfor
-                                    </div>
-                                </div>
+                                <x-insurance.insurance-rating-stars :score="$insurance->claim_ratings_avg_rating_score" />
                             </div>
                             <div>
                                 <span class="font-medium">Bewertungen:</span>
