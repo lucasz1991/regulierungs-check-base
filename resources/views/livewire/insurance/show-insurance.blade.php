537fcba9b8d9d7acbef56e1dfef4c77929d5f96a
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-08 05:24:38 +0200

    Enhance ShowInsurance component layout with improved spacing and updated ratings display

--- resources/views/livewire/insurance/show-insurance.blade.php
+++ resources/views/livewire/insurance/show-insurance.blade.php
@@ -15,14 +15,24 @@
                 <span class="text-gray-500">Keine Bewertungen</span>
             @endif
         </div>
-        <div class="mt-4">
+        <div class="mt-12">
             @if($insurance->ratings_count() > 0)
-            <h2 class="text-xl font-semibold mb-2">Bewertungen</h2>
+                <h2 class="flex items-center justify-center text-lg px-2 py-1 w-max mb-5">
+                    <span class="w-max">Bewertungen</span>
+                    <span class="ml-2 bg-white text-sky-600 text-xs shadow border border-sky-200 font-bold aspect-square px-2 py-1 flex items-center justify-center rounded-full h-7 leading-none">
+                        {{ $insurance->ratings_count() }}
+                    </span>
+                </h2>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                     @foreach($insurance->claimRatings as $claim_rating)
                         <div class="bg-white rounded-lg border border-gray-200 shadow p-4">
-                            <p class="text-gray-700">{{ $claim_rating->comment }}</p>
-                            <x-insurance.insurance-rating-stars :score="$claim_rating->rating_score" />
+                            
+                            <div class=" flex items-center content-center justify-between">
+                                <x-insurance.insurance-rating-stars :score="$claim_rating->rating_score" />
+                                <p class="text-gray-600 ">
+                                    {{ $claim_rating->created_at ? $claim_rating->created_at->diffForHumans() : 'Datum nicht verfügbar' }}
+                                </p>
+                            </div>
                         </div>
                     @endforeach
                 </div>
