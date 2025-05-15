f5a101a8210e8322fb44d05fc344ce207cb33334
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-15 10:34:06 +0200

    button ai

--- resources/views/livewire/tools/chatbot.blade.php
+++ resources/views/livewire/tools/chatbot.blade.php
@@ -104,7 +104,7 @@
                     <!-- Ladeanimation -->
                     <div x-show="isLoading" x-collapse  class=" flex gap-2 items-center text-gray-600 text-sm">
                         <div class="p-2 mb-4">
-                            <div class="p-2 rounded-md bg-[#d69bff2e] text-gray-600 w-max max-w-xs mt-2 ml-2 flex gap-5 items-center" >
+                            <div class="p-2 rounded-md bg-[#9aceff2e] text-gray-600 w-max max-w-xs mt-2 ml-2 flex gap-5 items-center" >
                                 <strong>{{ $assistantName }}:</strong> 
                                 <span class=""><svg class="mr-3 -ml-1 h-5 w-5 animate-spin text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg></span>
                             </div>
@@ -130,8 +130,8 @@
             
                 <!-- Eingabe & Buttons -->
                 <div class="relative w-full">
-                        <input x-model="messagefront" @keydown.enter="sendMessage()" class="w-full border-outline bg-secondary border border-outline rounded-radius rounded-lg px-2 py-2  pr-24 text-md text-on-surface focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 "  placeholder="Frage stellen..." />
-                        <button @click="sendMessage()" class="absolute bg-blue-50 right-3 top-1/2 -translate-y-1/2 bg-primary rounded-radius px-2 py-1 text-xs tracking-wide text-on-primary transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 ">senden</button>
+                        <input x-model="messagefront" @keydown.enter="sendMessage()" class="w-full border-outline bg-white border border-outline rounded-radius rounded-lg px-2 py-2  pr-24 text-md text-on-surface focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:cursor-not-allowed disabled:opacity-75 "  placeholder="Frage stellen..." />
+                        <button @click="sendMessage()" class="absolute  right-3 top-1/2 -translate-y-1/2 bg-secondary rounded-radius px-2 py-1 text-xs tracking-wide text-on-primary transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 ">senden</button>
                 </div>
             </div>
         </div>
