2ba8cde98cfbcd09ea75b88d5fe8d93805af21d4
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-17 17:00:57 +0200

    vvhgh

--- resources/views/components/claim-rating/claim-rating-card.blade.php
+++ resources/views/components/claim-rating/claim-rating-card.blade.php
@@ -1,6 +1,19 @@
 <div class="bg-blue-50 p-4 rounded shadow  h-full" >
     <div>
+        <div class="flex gap-4">
+            <div class="w-14 flex-none ">
+                <div class=" w-min rounded-xl flex items-center justify-center text-white text-base  px-2" style="background-color: {{ $rating->insurance->color ?? '#ccc' }};">
+                    {{ strtoupper(substr( $rating->insurance->initials, 0 ,4)) }}
+                </div>
+            </div>
+            <div class="grow">
+                <h2 class="text-base break-words ">
+                    {{ substr( $rating->insurance->name, 0 ,25) }}
+                </h2>
+            </div>
+        </div>
     </div>
+    <hr class="my-2">
     <div class="flex justify-between items-center">
         <x-insurance.insurance-rating-stars :score="$rating->rating_score" />
         <div class="text-sm text-gray-500">
