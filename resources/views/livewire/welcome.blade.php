80cbc8340851e080d21b20b04239e7f83520ca41
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-20 10:46:02 +0200

    sdsds

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,9 +1,9 @@
 <div  wire:loading.class="cursor-wait">
-  <section class="relative min-h-[40vh] xs:min-h-[60vh] grid content-center overflow-hidden" >
+  <section class="relative home-banner grid content-center overflow-hidden" >
     <img  class="h-full w-full !object-center !object-cover absolute" src="/site-images/background.jpg" alt="">
     <div class="absolute inset-0 bg-blue-100 opacity-50">
     </div>
-    <div class="max-w-4xl mx-auto text-center space-y-4 py-6 xs:py-16 px-6 md:px-12 relative h-max z-10">
+    <div class="max-w-4xl mx-auto text-center space-y-4 py-6  px-6 md:px-12 relative h-max z-10">
       <h2 class="text-3xl md:text-4xl  text-gray-800 mb-4 mt-5">
         Bewerte deine Versicherung
       </h2>
