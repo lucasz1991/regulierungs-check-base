ce8bd9be17aa4dc190a8b4f126b04b541eccc88d
Lucas Zacharias <lucas@zacharias-net.de>
2025-06-07 07:55:50 +0200

    Fix formatting in page header component for improved readability

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,12 +1,10 @@
     @if ($isWebPage && $showHeader)
-        <header class="relative bg-cover bg-center min-h-32  px-8 " 
-        style="background-image: url('{{ $header_image ? url('storage/' . $header_image) : asset('site-images/background.jpg') }}');">
+        <header class="relative bg-cover bg-center min-h-32  px-8 " style="background-image: url('{{ $header_image ? url('storage/' . $header_image) : asset('site-images/background.jpg') }}');">
             <div class="absolute inset-0 bg-blue-50 opacity-70"></div>
             <div class="relative container mx-auto py-12 text-2xl  space-x-6 flex justify-start  items-center">
                 <x-back-button />
                 <h1 class="font-semibold text-2xl text-gray-800 leading-tight flex items-center">
                     {{ $title }}
-                    
                     @if (!empty($icon))
                         <div class="pageheader-icon w-12 aspect-square text-[#333] ml-10  inline opacity-30">
                             {!! $icon !!}
