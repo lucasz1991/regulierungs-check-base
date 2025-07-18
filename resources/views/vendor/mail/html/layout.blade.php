306d08763c7d9e8a546c1b6cbd0cc6f9f427581a
Lucas Zacharias <lucas@zacharias-net.de>
2025-07-18 15:02:56 +0200

    sdsd

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -25,33 +25,35 @@ width: 100% !important;
 </style>
 </head>
 <body>
+<div style="background-color:#E7E7E7;">
 
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td align="center">
-<table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-{{ $header ?? '' }}
-
-<!-- Email Body -->
-<tr>
-<td class="body" width="100%" cellpadding="0" cellspacing="0" style="border: hidden !important;">
-<table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-<!-- Body content -->
-<tr>
-<td class="content-cell">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-
-{{ $subcopy ?? '' }}
-</td>
-</tr>
-</table>
-</td>
-</tr>
-
-{{ $footer ?? '' }}
-</table>
-</td>
-</tr>
-</table>
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+    <td align="center">
+    <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    {{ $header ?? '' }}
+    
+    <!-- Email Body -->
+    <tr>
+    <td class="body" width="100%" cellpadding="0" cellspacing="0" style="border: hidden !important;">
+    <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+    <!-- Body content -->
+    <tr>
+    <td class="content-cell">
+    {{ Illuminate\Mail\Markdown::parse($slot) }}
+    
+    {{ $subcopy ?? '' }}
+    </td>
+    </tr>
+    </table>
+    </td>
+    </tr>
+    
+    {{ $footer ?? '' }}
+    </table>
+    </td>
+    </tr>
+    </table>
+</div>
 </body>
 </html>
