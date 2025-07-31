e388df1ab4567c94333bc8a27dc522ea22afecb5
Lucas Zacharias <lucas@zacharias-net.de>
2025-07-31 05:06:59 +0200

    ass

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -95,14 +95,14 @@
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="name" value="{{ __('Benutzername') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
+            <x-input id="name" type="text" class="mt-2 block w-full" wire:model="state.name" required autocomplete="name" />
             <x-input-error for="name" class="mt-2" />
         </div>
 
         <!-- E-Mail -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="email" value="{{ __('E-Mail') }}" />
-            <x-input id="email" type="email" class="mt-1 block w-full @if ($this->user->hasVerifiedEmail()) ring  !ring-offset-4 !ring-green-200  @endif" wire:model="state.email" required autocomplete="username" />
+            <x-input id="email" type="email" class="mt-2 block w-full @if ($this->user->hasVerifiedEmail()) ring  !ring-offset-4 !ring-green-200  @endif" wire:model="state.email" required autocomplete="username" />
             <x-input-error for="email" class="mt-2" />
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) && ! $this->user->hasVerifiedEmail())
