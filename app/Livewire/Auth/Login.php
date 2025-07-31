25e986760d5e40f616f4bc95feef0b87dca8327d
Lucas Zacharias <lucas@zacharias-net.de>
2025-07-31 04:36:11 +0200

    sdsd

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -43,7 +43,7 @@ class Login extends Component
             ]);
         }
         $this->dispatch('showAlert','Willkommen zurück!', 'success');
-        return $this->redirect('/dashboard');
+        return $this->redirect()->intended(route('dashboard'));
     }
 
     public function mount()
