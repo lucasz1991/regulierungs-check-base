139de3c747e39b04837c3d235e18808251d09ebf
Lucas Zacharias <lucas@zacharias-net.de>
2025-09-23 20:06:38 +0200

    sdsd

--- resources/views/livewire/banner/top-insurances-banner.blade.php
+++ resources/views/livewire/banner/top-insurances-banner.blade.php
@@ -30,7 +30,7 @@
             <div class="swiper" x-ref="topSwiper">
                 <div class="swiper-wrapper">
                     @foreach ($insurances as $insurance)
-                        <div class="swiper-slide pr-4" wire:key="top-insurance-{{ $insurance->id }}">
+                        <div class="swiper-slide pr-8" wire:key="top-insurance-{{ $insurance->id }}">
                             <x-insurance.insurance-card-startswiper :insurance="$insurance" :isSubTypeFilter="$isSubTypeFilter" :subTypeFilterSubType="$subTypeFilterSubType" />
                         </div>
                     @endforeach
