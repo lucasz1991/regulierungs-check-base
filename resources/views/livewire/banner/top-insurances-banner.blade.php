7f9588675d775299afbe6511a9d8cad978d1cba0
Lucas Zacharias <lucas@zacharias-net.de>
2025-10-03 17:20:50 +0200

    Refactor insurance card link structure in top-insurances-banner for improved readability and consistency

--- resources/views/livewire/banner/top-insurances-banner.blade.php
+++ resources/views/livewire/banner/top-insurances-banner.blade.php
@@ -35,28 +35,27 @@
                             <x-insurance.insurance-card-startswiper :insurance="$insurance" :isSubTypeFilter="$isSubTypeFilter" :subTypeFilterSubType="$subTypeFilterSubType" />
                         </div>
                     @endforeach
-<div class="swiper-slide w-36 pr-4 h-full">
-    <a href="/insurances"
-       class="block h-full hover:shadow-lg cursor-pointer overflow-hidden rounded shadow"
-       x-data="{ hover: false }"
-       x-cloak>
-        <div class="bg-white px-2 pt-8 pb-2 relative transition-shadow duration-300 flex flex-col justify-center items-center h-full"
-             x-on:mouseenter="hover = true"
-             x-on:mouseleave="hover = false">
-            <div class="w-16 h-16 rounded-full bg-gray-200 hover:bg-blue-100 transition-all duration-200 flex items-center justify-center  mt-6">
-                <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" >
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                </svg>
-            </div>
-            <div class="transition-all duration-200 text-center h-full flex items-end justify-center" style="margin-top: 2px;">
-                <div class="h-16  flex items-end justify-center text-xs   font-medium  text-gray-600 ">
-                    weitere Anbieter vergleichen
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
-
+                    <div class="swiper-slide w-36 pr-4 h-full">
+                        <a href="/insurances"
+                        class="block h-full hover:shadow-lg cursor-pointer overflow-hidden rounded shadow"
+                        x-data="{ hover: false }"
+                        x-cloak>
+                            <div class="bg-white px-2 pt-8 pb-2 relative transition-shadow duration-300 flex flex-col justify-center items-center h-full"
+                                x-on:mouseenter="hover = true"
+                                x-on:mouseleave="hover = false">
+                                <div class="w-16 h-16 rounded-full bg-gray-200 hover:bg-blue-100 transition-all duration-200 flex items-center justify-center  mt-6">
+                                    <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" >
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                                    </svg>
+                                </div>
+                                <div class="transition-all duration-200 text-center h-full flex items-end justify-center" style="margin-top: 2px;">
+                                    <div class="h-16  flex items-end justify-center text-xs   font-medium  text-gray-600 ">
+                                        weitere Anbieter vergleichen
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
