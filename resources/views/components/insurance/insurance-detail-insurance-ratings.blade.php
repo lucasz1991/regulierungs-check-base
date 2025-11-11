8d7608152b22eb249773d639dcbee28b66ac9fd8
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-11 19:37:48 +0100

    jhjhj

--- resources/views/components/insurance/insurance-detail-insurance-ratings.blade.php
+++ resources/views/components/insurance/insurance-detail-insurance-ratings.blade.php
@@ -1,16 +1,16 @@
 <div class="lg:flex gap-4 mt-12">
-    <div class="bg-primary-50 p-4 rounded w-full lg:w-2/3">
+    <div class="relative bg-primary-50 p-4 rounded w-full lg:w-2/3">
         <div class="text-sm text-gray-600 mb-4">
             <strong class="my-4">Gesammt Scoring:</strong> 
-            <x-insurance.insurance-rating-stars :score="$detailInsuranceRating->total_score"  :size="'lg'"/>
+            <div class="mt-2 w-max">
+                <x-insurance.insurance-rating-stars :score="$detailInsuranceRating->total_score"  :size="'lg'"/>
+            </div>
         </div>
         <div class="prose max-w-full">
             <h2 class="text-lg font-semibold mb-2">Kommentar</h2>
             <p>{{ $detailInsuranceRating->ai_comment ?: 'Kein Kommentar vorhanden.' }}</p>
         </div>
-    </div>
-    <div class="bg-white p-4 rounded w-full lg:w-1/3  items-center">
-        <div>
+        <div class="absolute right-0 top-0 px-4">
             <div class="shrink-0 transition-all relative self-auto" >
                 <div  class="">
                     <div class="mt-5">
@@ -21,6 +21,9 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="bg-white p-4 rounded w-full lg:w-1/3  items-center">
+        
         <div label="Regulations Dauer">
             <div class="flex items-center justify-between space-x-2">
                 <span class="mr-4">Dauer:</span>
