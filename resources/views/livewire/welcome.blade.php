81ed9895b405f769c2bcd5936a3a03e08ac12cfd
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-08 09:31:20 +0100

    sdsd

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,8 +1,8 @@
 <div  wire:loading.class="cursor-wait">
-      <div class="container mx-auto px-4 py-6">
-        <div id="ix1xm-2" class="relative overflow-hidden rounded-xl home-banner px-4 py-4">
+      <div class="container mx-auto px-4 py-2 md:py-6">
+        <div id="ix1xm-2" class="relative overflow-hidden rounded-xl home-banner px-4 md:py-4">
           <div>
-            <div id="ib4xx-2" data-aos="fade-right" class="max-w-full md:max-w-3xl text-center md:text-left space-y-4 md:py-6  relative h-max z-10 py-4">
+            <div id="ib4xx-2" data-aos="fade-right" class="max-w-full md:max-w-3xl text-center md:text-left space-y-4 md:py-6  relative h-max z-10 md:py-4">
               <div id="ijooq">
                 <h1 id="ilifi-2" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                   Versicherungen im Bonitäts-CHECK.
@@ -15,9 +15,9 @@
             </div>
           </div>
           <div class="">
-            <div data-aos="fade-right" class="max-w-full md:max-w-3xl text-center md:text-left space-y-4 md:py-6  relative h-max z-10 py-6">
+            <div data-aos="fade-right" class="max-w-full md:max-w-3xl text-center md:text-left space-y-4  relative h-max z-10 py-2 md:py-6">
               <div>
-                <div  class="flex space-x-2 md:space-x-4 items-center mb-4 w-full max-md:justify-center">
+                <div  class="flex space-x-2 md:space-x-4 items-center md:mb-4 w-full max-md:justify-center">
                   <x-buttons.button-basic class="font-bold" :mode="'layoutgold'" x-on:click="Livewire.dispatch('showFormModal'); isClicked = true; setTimeout(() => isClicked = false, 100)">
                     Jetzt bewerten
                   </x-buttons.button-basic>
