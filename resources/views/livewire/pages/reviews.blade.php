72a6543a45677ff9ed6931cb6df91db2e9dda010
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-19 09:04:27 +0100

    ssd

--- resources/views/livewire/pages/reviews.blade.php
+++ resources/views/livewire/pages/reviews.blade.php
@@ -15,7 +15,7 @@
     }"
 >
     <div class="">
-        <div class="container mx-auto ">
+        <div class="container mx-auto px-4">
             {{-- Einzelbewertungen --}}
                 <x-filter.filter-container>
                     <x-slot name="filters">
