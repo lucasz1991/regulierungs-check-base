87777242632097c123c49d4612d7f4c5adfe54e1
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-21 20:29:32 +0100

    sdsdsd

--- resources/views/livewire/tools/chatbot.blade.php
+++ resources/views/livewire/tools/chatbot.blade.php
@@ -329,7 +329,7 @@
             class="
                 mt-2
                 flex justify-end
-                absolute right-2 bottom-0 pb-2
+                absolute right-2 bottom-2 
             "
         >
             <button
