cbf543d4cbf64bbcb758281a35745692f815db10
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-23 20:42:58 +0100

    sdsddd

--- resources/views/livewire/insurance/show-insurance.blade.php
+++ resources/views/livewire/insurance/show-insurance.blade.php
@@ -272,47 +272,47 @@
                                             <span>Übersicht</span>
                                         </h3>
                                         <div x-data="{ show: false }" class="relative">
-    <!-- Info-Icon -->
-    <button
-        @mouseenter="show = true"
-        @mouseleave="show = false"
-        @click.away="show = false"
-        @click.prevent="show = true"
-        x-ref="anchor"
-        class="text-gray-400 hover:text-gray-600 focus:outline-none -mt-4"
-        aria-label="Hinweis zur Logo-Nutzung"
-        type="button"
-    >
-        <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20.5C6.75 20.5 2.5 16.25 2.5 11S6.75 1.5 12 1.5 21.5 5.75 21.5 11 17.25 20.5 12 20.5z" />
-        </svg>
-    </button>
-    <!-- Hinweis-Box -->
-    <div
-        x-show="show"
-        x-transition
-         x-anchor.offset.10="$refs.anchor"
-        x-cloak
-        class="w-64 z-50 bg-gray-100 text-sm text-gray-800 rounded shadow-lg p-3"
-    >
-        <p class="leading-snug">
-        Angezeigt ist der Gesamtwert aller Sparten.
-Für die ⌀ Auszahlungsdauer je Sparte bitte den Filter nutzen.
-        </p>
-    </div>
-</div>
+                                            <!-- Info-Icon -->
+                                            <button
+                                                @mouseenter="show = true"
+                                                @mouseleave="show = false"
+                                                @click.away="show = false"
+                                                @click.prevent="show = true"
+                                                x-ref="anchor"
+                                                class="text-gray-400 hover:text-gray-600 focus:outline-none -mt-4"
+                                                aria-label="Hinweis zur Logo-Nutzung"
+                                                type="button"
+                                            >
+                                                <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M12 20.5C6.75 20.5 2.5 16.25 2.5 11S6.75 1.5 12 1.5 21.5 5.75 21.5 11 17.25 20.5 12 20.5z" />
+                                                </svg>
+                                            </button>
+                                            <!-- Hinweis-Box -->
+                                            <div
+                                                x-show="show"
+                                                x-transition
+                                                x-anchor.offset.10="$refs.anchor"
+                                                x-cloak
+                                                class="w-64 z-50 bg-gray-100 text-sm text-gray-800 rounded shadow-lg p-3"
+                                            >
+                                                <p class="leading-snug">
+                                                Angezeigt ist der Gesamtwert aller Sparten.
+                                                Für die ⌀ Auszahlungsdauer je Sparte bitte den Filter nutzen.
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
 
                                     <div class="grid grid-cols-1 gap-2">
                                         {{-- KPI 1 --}}
-                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-3">
+                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-2">
                                             <div class="flex items-start justify-between gap-4">
                                                 <p class="text-sm text-gray-700 flex items-center gap-2">
                                                     <i class="fal fa-clock text-primary fa-lg"></i>
                                                     Ø Regulierungsdauer 
                                                 </p>
 
-                                                <div class="relative  w-16 h-16  shrink-0">
+                                                <div class="relative  w-18 h-18  shrink-0">
                                                     <div class="absolute inset-0 rounded-full bg-gray-100"></div>
                                                     <div
                                                         class="absolute inset-0 rounded-full"
@@ -327,14 +327,14 @@ Für die ⌀ Auszahlungsdauer je Sparte bitte den Filter nutzen.
                                         </div>
 
                                         {{-- KPI 2 --}}
-                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-3">
+                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-2">
                                             <div class="flex items-start justify-between gap-4">
                                                 <p class="text-sm text-gray-700 flex items-center gap-2">
                                                     <i class="fal fa-star text-amber-500 fa-lg"></i>
                                                     Gesamtbewertung
                                                 </p>
 
-                                                <div class="relative  w-16 h-16  shrink-0">
+                                                <div class="relative  w-18 h-18  shrink-0">
                                                     <div class="absolute inset-0 rounded-full bg-gray-100"></div>
                                                     <div
                                                         class="absolute inset-0 rounded-full"
@@ -350,14 +350,14 @@ Für die ⌀ Auszahlungsdauer je Sparte bitte den Filter nutzen.
                                         </div>
 
                                         {{-- KPI 3 --}}
-                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-3">
+                                        <div class="rounded-2xl bg-white/95 border border-white/10 shadow p-2">
                                             <div class="flex items-start justify-between gap-4">
                                                 <p class="text-sm text-gray-700 flex items-center gap-2">
                                                     <i class="fal fa-comments text-emerald-500 fa-lg"></i>
                                                     Bewertungen
                                                 </p>
 
-                                                <div class="relative  w-16 h-16  shrink-0">
+                                                <div class="relative  w-18 h-18  shrink-0">
                                                     <div class="absolute inset-0 rounded-full bg-gray-100"></div>
                                                     <div
                                                         class="absolute inset-0 rounded-full"
