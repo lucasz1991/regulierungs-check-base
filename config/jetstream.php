1ba45251d81330da5cc098100fb21268b633ad7d
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-04 05:58:22 +0100

    ddfdf

--- config/jetstream.php
+++ config/jetstream.php
@@ -61,9 +61,9 @@ return [
         
         // Features::termsAndPrivacyPolicy(),
         Features::profilePhotos(),
-        Features::api(),
-        Features::teams(['invitations' => true]),
-        Features::accountDeletion(),
+        // Features::api(),
+        // Features::teams(['invitations' => true]),
+        // Features::accountDeletion(),
     ],
 
     /*
